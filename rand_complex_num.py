cbc1d25fb37d0e81df6d7c6785755acffff91e6f
Auten Lunsford <50330574+autenl@users.noreply.github.com>
2023-09-07 13:30:33 -0400

    Update rand_complex_num.py

--- rand_complex_num.py
+++ rand_complex_num.py
@@ -9,4 +9,4 @@ def rand_complex_num(real_lb, real_ub, im_lb, im_ub, amount=1):
     if amount == 1:
         return ComplexNumber(random_reals[0], random_ims[0])
     else:
-        return [ComplexNumber(random_reals[i], random_ims[i]) for i in range(0, amount - 1)]
+        return [ComplexNumber(random_reals[i], random_ims[i]) for i in range(0, amount)]
