7f9e182b8768227038e0da1cb4588c85b84578b5
Auten Lunsford <50330574+autenl@users.noreply.github.com>
2023-09-20 15:37:22 -0400

    Update rand_complex_num.py

--- rand_complex_num.py
+++ rand_complex_num.py
@@ -1,10 +1,11 @@
 import random
+import numpy as np
 from ComplexNumber import ComplexNumber
 
 
 def rand_complex_num(real_lb, real_ub, im_lb, im_ub, amount=1):
-    random_reals = random.sample(range(real_lb, real_ub + 1), amount)
-    random_ims = random.sample(range(im_lb, im_ub + 1), amount)
+    random_reals = np.random.uniform(real_lb, real_ub, [1, amount])[0]
+    random_ims = np.random.uniform(im_lb, im_ub, [1, amount])[0]
 
     if amount == 1:
         return ComplexNumber(random_reals[0], random_ims[0])
